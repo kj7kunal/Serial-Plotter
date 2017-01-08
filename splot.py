a1dcbd803b8c635417f66505276eecbd8b2030a7
kj7kunal <jain.kunal35@ymail.com>
2017-01-09 00:03:03 +0530

    Change plotter file to plot n data

--- splot.py
+++ splot.py
@@ -1,66 +1,70 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat Dec 24 20:38:39 2016
+
 @author: Kunal
 """
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+#from matplotlib import style
 import time
-
-x=range(0,50)
-y = [[0]*50 for _ in range(2)]	#because list is mutable 
-yval = [0, 0]
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.close()
 ser.open()
+time.sleep(1)
 
+x=range(0,50)
+dataline = ser.readline()
+yval = [int(s) for s in dataline.split(' ')]
+n = len(yval)
+y = [[0]*50 for _ in range(n)]	#because list is mutable 
 
+#style.use('fivethirtyeight')
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.set_title('Pitch vs time')
 ax.grid(True)
 ax.set_ylabel('Pitch')
 ax.set_xlabel('time')
+
 lines=[]
-clr = ["red","cyan"]
-for index in range(2):
-    lobj = ax.plot([],[],lw=2,color=clr[index])[0]
+for index in range(n):
+    lobj = ax.plot([],[])[0] 
     lines.append(lobj)
+
 def init():
     for line in lines:
         line.set_data([],[])
     return lines
 
-
 def update(i):
+    global n
     del x[0]    
     x.append(x[len(x)-1]+1)
     dataline = ser.readline()
-    time.sleep(0.05)
+    time.sleep(0.01)
     yval = [int(s) for s in dataline.split(' ')]
     
-    for j in range(2):
+    for j in range(n):
         del y[j][0]
         y[j].append(yval[j])
         lines[j].set_data(x,y[j])
 
-    print yval, y
-
     plt.ylim([np.min(y)-10,np.max(y)+10])
     plt.xlim([x[0],x[len(x)-1]])
     plt.draw()
-    time.sleep(0.05)
 
     return lines,
-
+    
+            
 try:
-    anim = animation.FuncAnimation(fig,update,init_func=init,interval=50)
+    anim = animation.FuncAnimation(fig,update,init_func=init,interval=20)
     # show plot
     plt.show() 
 except ValueError:
-    print "boooo"
     ser.close()
 except KeyboardInterrupt:
     ser.close()
